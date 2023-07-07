28e9fa2d0546270d8d7157d2b462d5b0dd4e55e3
borsochek <idievafaina@gmail.com>
2023-07-07 19:49:16 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int ticket = 19654; //  стоимость билета
+        int percent = 1 * 20; //  из расчета 1 миля за каждые 20 рублей
+
+        System.out.println((ticket / percent) + " сумма миль, которые начислятся  ");
+
+        int miles = 982;
+
+    }
+}
